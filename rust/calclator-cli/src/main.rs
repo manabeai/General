7ed5fda9843607e35434341002a1d5df1b6dfa80
manabeai <matsu.devtool@gmail.com>
2024-12-10 12:24:09 +0900

    数字を複数受け取れる

--- rust/calclator-cli/src/main.rs
+++ rust/calclator-cli/src/main.rs
@@ -3,12 +3,10 @@ use clap::{Parser, ValueEnum};
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-
     #[arg(value_enum)]
     mode: Mode,
 
-    num1: u64,
-    num2: u64,
+    nums: Vec<i32>,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -22,12 +20,18 @@ fn main() {
 
     match cli.mode {
         Mode::Add => {
-            let sum = cli.num1 + cli.num2;
+            let sum = cli.nums.iter().sum::<i32>();
             println!("Sum: {}", sum);
         }
         Mode::Min => {
-            let difference = cli.num1.saturating_sub(cli.num2);
+            let difference = cli.nums[0] - cli.nums.iter().skip(1).sum::<i32>();
             println!("Difference: {}", difference);
         }
     }
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
