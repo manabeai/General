ceab7b9454321c9dcff4097638dc506bbcdf06b1
manabeai <matsu.devtool@gmail.com>
2024-12-27 12:45:04 +0900

    コントローラーを追加

--- rails/update-rails/db/seeds.rb
+++ rails/update-rails/db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# ユーザーを3人作成
+users = []
+3.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    created_at: Faker::Time.backward(days: 30),
+    updated_at: Faker::Time.backward(days: 20)
+  )
+  users << user
+end
+
+# ツイートを10件作成
+10.times do
+  Tweet.create!(
+    content: Faker::Lorem.sentence(word_count: 10),
+    created_at: Faker::Time.backward(days: 15),
+    updated_at: Faker::Time.backward(days: 5),
+    user_id: users.sample.id # ランダムなユーザーを紐付け
+  )
+end
+
+puts "Seedデータの作成が完了しました！"
+puts "Users: #{User.count}, Tweets: #{Tweet.count}"
--- rails/update-rails/spec/factories/tweets.rb
+++ rails/update-rails/spec/factories/tweets.rb
@@ -1,5 +1,9 @@
+# spec/factories/tweets.rb
 FactoryBot.define do
   factory :tweet do
-    
+    content { Faker::Lorem.sentence(word_count: 10) }
+    created_at { Faker::Time.backward(days: 7) }
+    updated_at { Faker::Time.backward(days: 1) }
+    association :user
   end
 end
